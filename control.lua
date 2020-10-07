3ed51171209439f957884d2c8f63db03973eb1af
Christoph Schmeisser <christoph@schmeisserweb.at>
2020-10-08 00:17:00 +0200

    Handle upgrades using a separate table.

--- control.lua
+++ control.lua
@@ -1,18 +1,13 @@
--- Helper function
--- local function debug_print(str)
--- 	if global.poflo_debug
--- 	then
--- 		game.print(str)
--- 	end
--- end
+-- Table to keep track of upgrades. The built-in function is unreliable.
+global.upgrades = global.upgrades or {}
+
 
 
 -- Produces a selection tool and takes it away again.
 local function on_hotkey_main(event)
-
-	local player = game.players[event.player_index]
-
-	-- once in their life, a message is displayed giving a hint	
+  local player = game.players[event.player_index]
+  
+	-- once in a save game, a message is displayed giving a hint	
 	global.bprio_hint = global.bprio_hint or 0	
 	if global.bprio_hint == 0
 	then
@@ -36,23 +31,60 @@ local function on_hotkey_main(event)
       cursor_stack.set_stack({name="bot-prioritizer", type="selection-tool", count = 1})
 		end
 	end
-
-
 end
 
 -- Start it from shortcut instead of hotkey
 local function bot_prio_shortcut(event)
-	-- debug_print("Bot Prio Shortcut")
-	if event.prototype_name == "bot-prio-shortcut"
-  then
+	if event.prototype_name == "bot-prio-shortcut" then
 		on_hotkey_main(event)
 	end
 end
 
+-- Add information about upgrades to our table
+local function handle_ordered_upgrades(event)
+  local ent = event.entity
+  local nr = event.entity.unit_number
+  local trg = event.target.name
+
+  if global.upgrades then
+    global.upgrades = global.upgrades
+  else
+    global.upgrades = {}
+  end
+  if not global.upgrades[nr] then
+    global.upgrades[nr] = { e = ent, t = trg }
+  end
+
+end
+
+-- Remove upgrades from our table if they have been cancelled
+local function handle_cancelled_upgrades(event)
+  local nr = event.entity.unit_number
+
+  if global.upgrades and global.upgrades[nr] then
+    global.upgrades[nr] = nil
+  end
+end
+
+-- Clear stale entites from upgrade table
+local function remove_stale_upgrades()
+  if global.upgrades then
+    global.upgrades = global.upgrades
+  else
+    global.upgrades = {}
+  end
+
+  for unit_nr, data in pairs(global.upgrades) do
+    if not data.e.valid or not data.e or not data.e.to_be_upgraded then
+      global.upgrades[unit_nr] = nil
+    end
+  end
+end
+
+
 -- Runs after player selected stuff
 local function handle_selection(event)
   local function_name = "botprio event"
-
   if not event.item == 'bot-prioritizer' then return end
 
   -- Main logic
@@ -68,6 +100,9 @@ local function handle_selection(event)
   player.remove_item({name = 'bot-prioritizer'})
   -- force.chart(surface, area)
 
+  -- Keep updgrade table clean
+  remove_stale_upgrades()
+
   for _, entity in ipairs(entities) do
     if entity.type == "entity-ghost" or entity.type == "tile-ghost" then -- handle ghosts
       local new = entity.clone({position = entity.position, force = entity.force})
@@ -77,20 +112,26 @@ local function handle_selection(event)
       entity.cancel_deconstruction(force)
       entity.order_deconstruction(force)
     elseif entity ~= nil and entity.to_be_upgraded() then -- handle upgrades
-      local upg_target = entity.get_upgrade_target()
-      entity.cancel_upgrade(force)
-      entity.order_upgrade({force = entity.force, target = upg_target, player = player})
+      local up_trg = global.upgrades[entity.unit_number].t
+      if up_trg ~= nil then
+        entity.cancel_upgrade(force)
+        entity.order_upgrade({force = entity.force, target = up_trg, player = player})
+      else
+        player.print("ERROR: Couldn't find out upgrade target.")
+      end
     end
   end
-
-
 end
 
 
 -- Hotkey
-script.on_event( "botprio-hotkey", on_hotkey_main )
+script.on_event( "botprio-hotkey", on_hotkey_main)
 -- Shortcut button
-script.on_event( defines.events.on_lua_shortcut, bot_prio_shortcut )
+script.on_event( defines.events.on_lua_shortcut, bot_prio_shortcut)
 
 -- Gather entity ghosts and give bots priority after selction is made
 script.on_event(defines.events.on_player_selected_area, handle_selection)
+
+-- Handle upgrade orders because get_upgrade_target() is unreliable
+script.on_event(defines.events.on_marked_for_upgrade, handle_ordered_upgrades)
+script.on_event(defines.events.on_cancelled_upgrade, handle_cancelled_upgrades)
