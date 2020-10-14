a0181b6307a50589caf026c8c714eef07eb34011
Christoph Schmeisser <christoph@schmeisserweb.at>
2020-10-14 18:33:29 +0200

    New setting for the time the history is kept per player

--- settings.lua
+++ settings.lua
@@ -14,10 +14,18 @@ data:extend({
       order = 'a[botprio]-b'
     },
     {
+      type = "int-setting",
+      name = "botprio-toggling-time",
+      setting_type = "runtime-per-user",
+      default_value = 5,
+      allowed_values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30},
+      order = 'a[botprio]-c'
+    },
+    {
       type = "bool-setting",
       name = "botprio-disable-msg",
       setting_type = "runtime-per-user",
       default_value = false,
-      order = 'a[botprio]-c'
+      order = 'a[botprio]-d'
     }
   })
