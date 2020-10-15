76f32ff3b38024d21246a884e09dc0dc62626c52
Christoph Schmeisser <christoph@schmeisserweb.at>
2020-10-15 19:39:00 +0200

    Simplified circuit connection handling

--- circuit_manager.lua
+++ circuit_manager.lua
@@ -11,13 +11,9 @@ function circ_mgr.copy_circuit_connections(from_old, to_new)
 
     if from_old.circuit_connection_definitions then 
         for _, con_def in ipairs(from_old.circuit_connection_definitions) do -- Connections from entity in question
-            for __, trg_con_def in ipairs(con_def.target_entity.circuit_connection_definitions) do -- Connections of those connected entities
-                if (trg_con_def.target_entity == from_old) then -- is it the connection back to the entity in question?
-                    local c_d_to_new = hlp.tbl_deep_copy(trg_con_def) -- copy ConnectionDefinition
-                    c_d_to_new.target_entity = hlp.tbl_deep_copy(to_new) -- copy a new reference to the cloned entity as "target_entity"
-                    con_def.target_entity.connect_neighbour(c_d_to_new) -- connect!
-                end
-            end
+            local c_d_to_new = hlp.tbl_deep_copy(con_def) -- copy ConnectionDefinition
+            c_d_to_new.target_entity = hlp.tbl_deep_copy(to_new) -- copy a new reference to the cloned entity as "target_entity"
+            con_def.target_entity.connect_neighbour(c_d_to_new) -- Connect the former target (not the old entity) to the new one!
         end
     end
 end
