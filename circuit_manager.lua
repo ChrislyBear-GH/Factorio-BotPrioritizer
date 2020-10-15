4c3df6784ef5507acd70935dc143ee0caf2857d5
ChrislyBear-GH <christoph@schmeisserweb.at>
2020-10-15 12:53:18 +0200

    Bug: Need to check if entity has connections at all

--- circuit_manager.lua
+++ circuit_manager.lua
@@ -9,12 +9,14 @@ local hlp = require("helpers")
 -- NOT the other way around!
 function circ_mgr.copy_circuit_connections(from_old, to_new)
 
-    for _, con_def in ipairs(from_old.circuit_connection_definitions) do -- Connections from entity in question
-        for __, trg_con_def in ipairs(con_def.target_entity.circuit_connection_definitions) do -- Connections of those connected entities
-            if (trg_con_def.target_entity == from_old) then -- is it the connection back to the entity in question?
-                local c_d_to_new = hlp.tbl_deep_copy(trg_con_def) -- copy ConnectionDefinition
-                c_d_to_new.target_entity = hlp.tbl_deep_copy(to_new) -- copy a new reference to the cloned entity as "target_entity"
-                con_def.target_entity.connect_neighbour(c_d_to_new) -- connect!
+    if from_old.circuit_connection_definitions then 
+        for _, con_def in ipairs(from_old.circuit_connection_definitions) do -- Connections from entity in question
+            for __, trg_con_def in ipairs(con_def.target_entity.circuit_connection_definitions) do -- Connections of those connected entities
+                if (trg_con_def.target_entity == from_old) then -- is it the connection back to the entity in question?
+                    local c_d_to_new = hlp.tbl_deep_copy(trg_con_def) -- copy ConnectionDefinition
+                    c_d_to_new.target_entity = hlp.tbl_deep_copy(to_new) -- copy a new reference to the cloned entity as "target_entity"
+                    con_def.target_entity.connect_neighbour(c_d_to_new) -- connect!
+                end
             end
         end
     end
