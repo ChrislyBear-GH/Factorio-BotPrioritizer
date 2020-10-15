c7bbda1fb8826711b971dc0f0d640e8447e01c38
Christoph Schmeisser <christoph@schmeisserweb.at>
2020-10-15 18:40:24 +0200

    Catch error if item cannot be found. (Krastorio 2...)

--- inventory_helper.lua
+++ inventory_helper.lua
@@ -1,7 +1,12 @@
 local inv_hlp = {}
 
 function inv_hlp.in_inventory(player, entity_name)
-    return (player.get_main_inventory().get_item_count(entity_name)>0)
+    local cnt = 0
+    if pcall(function() cnt = player.get_main_inventory().get_item_count(entity_name) end) then
+        return (cnt>0)
+    else
+        return true
+    end
 end
 
 return inv_hlp
